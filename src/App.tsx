6fde2b1d1299fd12de0034b5e5317c0d7544e298
6-ON <aminhatim6@gmail.com>
2023-11-19 22:37:35 +0100

    Refactor App component to include snap-sync title and display multiple PostCards

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import PostForm from './components/Post/PostForm'
-import { Container, Stack } from '@mui/material'
+import { Container, Stack, Typography } from '@mui/material'
 import { useMediaQuery } from '@mui/material'
+import PostCard from './components/Post'
 
 function App() {
 	const isSmallScreen = useMediaQuery('(max-width: 600px)')
 
 	return (
-		<Stack direction={isSmallScreen ? 'column' : 'row'}>
-			<Container sx={{ backgroundColor: 'red', flex: 2 }}>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas odio possimus quis autem mollitia
-				laudantium incidunt quibusdam. Quisquam vitae error libero soluta voluptatibus veritatis ad, id, optio
-				placeat illum sit!
-			</Container>
-			<Container sx={{ position: 'relative', flex: 1 }}>
-				<PostForm />
-			</Container>
-		</Stack>
+		<>
+			<Typography variant="h1" textAlign={'center'}>snap-sync</Typography>
+			<Stack direction={isSmallScreen ? 'column' : 'row'}>
+				<Stack direction={'row'} sx={{ flex: 2 }} flexWrap={'wrap'} gap={'1rem'}>
+					{[...new Array(10)].map((_, i) => (
+						<PostCard key={i} />
+					))}
+				</Stack>
+				<Container sx={{ position: 'relative', flex: 1 }}>
+					<PostForm />
+				</Container>
+			</Stack>
+		</>
 	)
 }
 

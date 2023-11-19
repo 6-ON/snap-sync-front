64ed84478672a75ce269bb1489a565dfa8da2772
6-ON <aminhatim6@gmail.com>
2023-11-20 00:40:36 +0100

    Add Redux posts selector

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import PostForm from './components/Post/PostForm'
 import { Container, Stack, Typography } from '@mui/material'
 import { useMediaQuery } from '@mui/material'
 import PostCard from './components/Post'
+import { useAppSelector } from './redux/hooks'
 
 function App() {
 	const isSmallScreen = useMediaQuery('(max-width: 600px)')
-
+	const posts = useAppSelector((state) => state.post.posts)
 	return (
 		<>
 			<Typography variant="h1" textAlign={'center'}>snap-sync</Typography>
 			<Stack direction={isSmallScreen ? 'column' : 'row'}>
 				<Stack direction={'row'} sx={{ flex: 2 }} flexWrap={'wrap'} gap={'1rem'}>
-					{[...new Array(10)].map((_, i) => (
+					{posts.map((_, i) => (
 						<PostCard key={i} />
 					))}
 				</Stack>

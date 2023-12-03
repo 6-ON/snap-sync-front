e8d3396c84cfa1c0ce68c5e65114c8ec7be807f2
6-ON <aminhatim6@gmail.com>
2023-12-03 20:48:26 +0100

    add logout button , user image and fix responsive layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,28 @@
 import { FC } from 'react'
 import { LayoutProps } from './types'
 import { Link } from 'react-router-dom'
-import { Paper, Button } from '@mui/material'
-import { useAppDispatch, useAppSelector } from '../redux/hooks'
+import { Paper, Button, Stack, Avatar, Typography } from '@mui/material'
+import { useAppDispatch } from '../redux/hooks'
 import { logout } from '../redux/actions'
+import { useUser } from '../hooks/useUser'
 
 const MainLayout: FC<LayoutProps> = ({ children }) => {
-	const user = useAppSelector((state) => state.user.user)
+	const user = useUser()
 	const dispatch = useAppDispatch()
-	
+
 	return (
 		<>
-			<div className="flex justify-center items-center my-8 px-8 h-24">
+			<div className="flex justify-center items-center my-8 px-8 ">
 				<Paper
 					elevation={5}
 					sx={{
+						flexDirection: { xs: 'column', md: 'row' },
+						borderRadius: '1rem',
+						padding: '1rem',
 						width: '100%',
 						height: '100%',
 						textAlign: 'center',
-						justifyContent: 'center',
+						justifyContent: 'space-between',
 						alignItems: 'center',
 						display: 'flex',
 					}}
@@ -28,14 +32,26 @@ const MainLayout: FC<LayoutProps> = ({ children }) => {
 					</Link>
 					{!user ? (
 						<Link to="/login">
-							<Button variant="contained" color="success" sx={{ mr: 'auto' }}>
+							<Button variant="contained" color="success">
 								Login
 							</Button>
 						</Link>
 					) : (
-						<Button onClick={()=> {dispatch<any>(logout())}} variant="contained" color="success" sx={{ mr: 'auto' }}>
-							Logout
-						</Button>
+						<Stack direction={{ xs: 'column', md: 'row' }} gap={2}>
+							<Stack direction={{ xs: 'column', md: 'row' }} alignItems={'center'} gap={2}>
+								<Avatar src={user?.image}>{user.name.substring(0, 1)}</Avatar>
+								<Typography variant="h6">{user?.name}</Typography>
+							</Stack>
+							<Button
+								onClick={() => {
+									dispatch<any>(logout())
+								}}
+								variant="contained"
+								color="error"
+							>
+								Logout
+							</Button>
+						</Stack>
 					)}
 				</Paper>
 			</div>
